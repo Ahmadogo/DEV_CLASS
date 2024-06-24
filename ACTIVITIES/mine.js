5ba7c9973127b50a1cd48634b45e9d6bbe5c3edf
Abdulkareem <abdulkareemahmad413@gmail.com>
2024-06-24 14:48:57 +0100

    new files and pictures were added and a javascript fiile

---
+++ ACTIVITIES/mine.js
@@ -0,0 +1,7 @@
+document.getElementById('myh').textContent = 'Hii';
+document.getElementById('myp').textContent = 'My name is anas  isah';
+console.log('hello world');
+console.log('yoou are wellcome');
+
+// window.alert('you are in my page');
+window.alert('You are in danger');
